b3073f4cfd3edad03cc94e1712ed0e820901e640
Evolusen <31206344+Evolusen@users.noreply.github.com>
2017-09-07 23:20:55 +0200

    Add files via upload

---
+++ src/CMakeFiles/Serialization.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/evolusen/una/src/Serialization/BinaryInputStreamSerializer.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Serialization.dir/Serialization/BinaryInputStreamSerializer.cpp.o"
+  "/home/evolusen/una/src/Serialization/BinaryOutputStreamSerializer.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Serialization.dir/Serialization/BinaryOutputStreamSerializer.cpp.o"
+  "/home/evolusen/una/src/Serialization/JsonInputStreamSerializer.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Serialization.dir/Serialization/JsonInputStreamSerializer.cpp.o"
+  "/home/evolusen/una/src/Serialization/JsonInputValueSerializer.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Serialization.dir/Serialization/JsonInputValueSerializer.cpp.o"
+  "/home/evolusen/una/src/Serialization/JsonOutputStreamSerializer.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Serialization.dir/Serialization/JsonOutputStreamSerializer.cpp.o"
+  "/home/evolusen/una/src/Serialization/KVBinaryInputStreamSerializer.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Serialization.dir/Serialization/KVBinaryInputStreamSerializer.cpp.o"
+  "/home/evolusen/una/src/Serialization/KVBinaryOutputStreamSerializer.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Serialization.dir/Serialization/KVBinaryOutputStreamSerializer.cpp.o"
+  "/home/evolusen/una/src/Serialization/MemoryStream.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Serialization.dir/Serialization/MemoryStream.cpp.o"
+  "/home/evolusen/una/src/Serialization/SerializationOverloads.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Serialization.dir/Serialization/SerializationOverloads.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "STATICLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../external"
+  "version"
+  "../../src/Platform/Linux"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CMakeFiles/System.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/evolusen/una/src/Platform/Linux/System/Dispatcher.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/System.dir/Platform/Linux/System/Dispatcher.cpp.o"
+  "/home/evolusen/una/src/Platform/Linux/System/ErrorMessage.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/System.dir/Platform/Linux/System/ErrorMessage.cpp.o"
+  "/home/evolusen/una/src/Platform/Linux/System/Ipv4Resolver.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/System.dir/Platform/Linux/System/Ipv4Resolver.cpp.o"
+  "/home/evolusen/una/src/Platform/Linux/System/TcpConnection.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/System.dir/Platform/Linux/System/TcpConnection.cpp.o"
+  "/home/evolusen/una/src/Platform/Linux/System/TcpConnector.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/System.dir/Platform/Linux/System/TcpConnector.cpp.o"
+  "/home/evolusen/una/src/Platform/Linux/System/TcpListener.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/System.dir/Platform/Linux/System/TcpListener.cpp.o"
+  "/home/evolusen/una/src/Platform/Linux/System/Timer.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/System.dir/Platform/Linux/System/Timer.cpp.o"
+  "/home/evolusen/una/src/System/ContextGroup.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/System.dir/System/ContextGroup.cpp.o"
+  "/home/evolusen/una/src/System/ContextGroupTimeout.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/System.dir/System/ContextGroupTimeout.cpp.o"
+  "/home/evolusen/una/src/System/Event.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/System.dir/System/Event.cpp.o"
+  "/home/evolusen/una/src/System/EventLock.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/System.dir/System/EventLock.cpp.o"
+  "/home/evolusen/una/src/System/InterruptedException.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/System.dir/System/InterruptedException.cpp.o"
+  "/home/evolusen/una/src/System/Ipv4Address.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/System.dir/System/Ipv4Address.cpp.o"
+  "/home/evolusen/una/src/System/RemoteEventLock.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/System.dir/System/RemoteEventLock.cpp.o"
+  "/home/evolusen/una/src/System/TcpStream.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/System.dir/System/TcpStream.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "STATICLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../external"
+  "version"
+  "../../src/Platform/Linux"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
