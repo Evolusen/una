7379f6eb72b108716a04bc220c3697590699a016
Evolusen <31206344+Evolusen@users.noreply.github.com>
2017-09-07 21:32:11 +0200

    Add files via upload

---
+++ CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/evolusen/una
+# Build directory: /home/evolusen/una/build/release
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(external)
+subdirs(src)
+subdirs(tests)
