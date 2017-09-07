747ed78492223e02294cfb798c9dbed8a2b3ec5d
Evolusen <31206344+Evolusen@users.noreply.github.com>
2017-09-07 22:39:32 +0200

    Add files via upload

---
+++ src/CMakeFiles/Common.dir/DependInfo.cmake
@@ -0,0 +1,51 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/evolusen/una/src/Common/Base58.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/Base58.cpp.o"
+  "/home/evolusen/una/src/Common/BlockingQueue.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/BlockingQueue.cpp.o"
+  "/home/evolusen/una/src/Common/CommandLine.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/CommandLine.cpp.o"
+  "/home/evolusen/una/src/Common/ConsoleHandler.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/ConsoleHandler.cpp.o"
+  "/home/evolusen/una/src/Common/ConsoleTools.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/ConsoleTools.cpp.o"
+  "/home/evolusen/una/src/Common/IInputStream.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/IInputStream.cpp.o"
+  "/home/evolusen/una/src/Common/IOutputStream.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/IOutputStream.cpp.o"
+  "/home/evolusen/una/src/Common/JsonValue.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/JsonValue.cpp.o"
+  "/home/evolusen/una/src/Common/Math.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/Math.cpp.o"
+  "/home/evolusen/una/src/Common/MemoryInputStream.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/MemoryInputStream.cpp.o"
+  "/home/evolusen/una/src/Common/PathTools.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/PathTools.cpp.o"
+  "/home/evolusen/una/src/Common/ScopeExit.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/ScopeExit.cpp.o"
+  "/home/evolusen/una/src/Common/SignalHandler.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/SignalHandler.cpp.o"
+  "/home/evolusen/una/src/Common/StdInputStream.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/StdInputStream.cpp.o"
+  "/home/evolusen/una/src/Common/StdOutputStream.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/StdOutputStream.cpp.o"
+  "/home/evolusen/una/src/Common/StreamTools.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/StreamTools.cpp.o"
+  "/home/evolusen/una/src/Common/StringInputStream.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/StringInputStream.cpp.o"
+  "/home/evolusen/una/src/Common/StringOutputStream.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/StringOutputStream.cpp.o"
+  "/home/evolusen/una/src/Common/StringTools.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/StringTools.cpp.o"
+  "/home/evolusen/una/src/Common/StringView.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/StringView.cpp.o"
+  "/home/evolusen/una/src/Common/Util.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/Util.cpp.o"
+  "/home/evolusen/una/src/Common/VectorOutputStream.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Common.dir/Common/VectorOutputStream.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "STATICLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../external"
+  "version"
+  "../../src/Platform/Linux"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/CMakeFiles/Crypto.dir/DependInfo.cmake
@@ -0,0 +1,66 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/evolusen/una/src/crypto/blake256.c" "/home/evolusen/una/build/release/src/CMakeFiles/Crypto.dir/crypto/blake256.c.o"
+  "/home/evolusen/una/src/crypto/chacha8.c" "/home/evolusen/una/build/release/src/CMakeFiles/Crypto.dir/crypto/chacha8.c.o"
+  "/home/evolusen/una/src/crypto/crypto-ops-data.c" "/home/evolusen/una/build/release/src/CMakeFiles/Crypto.dir/crypto/crypto-ops-data.c.o"
+  "/home/evolusen/una/src/crypto/crypto-ops.c" "/home/evolusen/una/build/release/src/CMakeFiles/Crypto.dir/crypto/crypto-ops.c.o"
+  "/home/evolusen/una/src/crypto/groestl.c" "/home/evolusen/una/build/release/src/CMakeFiles/Crypto.dir/crypto/groestl.c.o"
+  "/home/evolusen/una/src/crypto/hash-extra-blake.c" "/home/evolusen/una/build/release/src/CMakeFiles/Crypto.dir/crypto/hash-extra-blake.c.o"
+  "/home/evolusen/una/src/crypto/hash-extra-groestl.c" "/home/evolusen/una/build/release/src/CMakeFiles/Crypto.dir/crypto/hash-extra-groestl.c.o"
+  "/home/evolusen/una/src/crypto/hash-extra-jh.c" "/home/evolusen/una/build/release/src/CMakeFiles/Crypto.dir/crypto/hash-extra-jh.c.o"
+  "/home/evolusen/una/src/crypto/hash-extra-skein.c" "/home/evolusen/una/build/release/src/CMakeFiles/Crypto.dir/crypto/hash-extra-skein.c.o"
+  "/home/evolusen/una/src/crypto/hash.c" "/home/evolusen/una/build/release/src/CMakeFiles/Crypto.dir/crypto/hash.c.o"
+  "/home/evolusen/una/src/crypto/jh.c" "/home/evolusen/una/build/release/src/CMakeFiles/Crypto.dir/crypto/jh.c.o"
+  "/home/evolusen/una/src/crypto/keccak.c" "/home/evolusen/una/build/release/src/CMakeFiles/Crypto.dir/crypto/keccak.c.o"
+  "/home/evolusen/una/src/crypto/oaes_lib.c" "/home/evolusen/una/build/release/src/CMakeFiles/Crypto.dir/crypto/oaes_lib.c.o"
+  "/home/evolusen/una/src/crypto/random.c" "/home/evolusen/una/build/release/src/CMakeFiles/Crypto.dir/crypto/random.c.o"
+  "/home/evolusen/una/src/crypto/skein.c" "/home/evolusen/una/build/release/src/CMakeFiles/Crypto.dir/crypto/skein.c.o"
+  "/home/evolusen/una/src/crypto/slow-hash.c" "/home/evolusen/una/build/release/src/CMakeFiles/Crypto.dir/crypto/slow-hash.c.o"
+  "/home/evolusen/una/src/crypto/tree-hash.c" "/home/evolusen/una/build/release/src/CMakeFiles/Crypto.dir/crypto/tree-hash.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STATICLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../external"
+  "version"
+  "../../src/Platform/Linux"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/evolusen/una/src/crypto/crypto.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Crypto.dir/crypto/crypto.cpp.o"
+  "/home/evolusen/una/src/crypto/slow-hash.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Crypto.dir/crypto/slow-hash.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "STATICLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../external"
+  "version"
+  "../../src/Platform/Linux"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
