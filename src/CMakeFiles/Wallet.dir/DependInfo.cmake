c94e6803afe4297c516a78141113cde41851058b
Evolusen <31206344+Evolusen@users.noreply.github.com>
2017-09-07 23:44:44 +0200

    Add files via upload

---
+++ src/CMakeFiles/Wallet.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/evolusen/una/src/Wallet/LegacyKeysImporter.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Wallet.dir/Wallet/LegacyKeysImporter.cpp.o"
+  "/home/evolusen/una/src/Wallet/WalletAsyncContextCounter.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Wallet.dir/Wallet/WalletAsyncContextCounter.cpp.o"
+  "/home/evolusen/una/src/Wallet/WalletErrors.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Wallet.dir/Wallet/WalletErrors.cpp.o"
+  "/home/evolusen/una/src/Wallet/WalletGreen.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Wallet.dir/Wallet/WalletGreen.cpp.o"
+  "/home/evolusen/una/src/Wallet/WalletRpcServer.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Wallet.dir/Wallet/WalletRpcServer.cpp.o"
+  "/home/evolusen/una/src/Wallet/WalletSerialization.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Wallet.dir/Wallet/WalletSerialization.cpp.o"
+  "/home/evolusen/una/src/Wallet/WalletUtils.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Wallet.dir/Wallet/WalletUtils.cpp.o"
+  "/home/evolusen/una/src/WalletLegacy/KeysStorage.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/KeysStorage.cpp.o"
+  "/home/evolusen/una/src/WalletLegacy/WalletHelper.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletHelper.cpp.o"
+  "/home/evolusen/una/src/WalletLegacy/WalletLegacy.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletLegacy.cpp.o"
+  "/home/evolusen/una/src/WalletLegacy/WalletLegacySerialization.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletLegacySerialization.cpp.o"
+  "/home/evolusen/una/src/WalletLegacy/WalletLegacySerializer.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletLegacySerializer.cpp.o"
+  "/home/evolusen/una/src/WalletLegacy/WalletTransactionSender.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletTransactionSender.cpp.o"
+  "/home/evolusen/una/src/WalletLegacy/WalletUnconfirmedTransactions.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletUnconfirmedTransactions.cpp.o"
+  "/home/evolusen/una/src/WalletLegacy/WalletUserTransactionsCache.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/Wallet.dir/WalletLegacy/WalletUserTransactionsCache.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "STATICLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../external"
+  "version"
+  "../../src/Platform/Linux"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
