127b6b00283f9edcb773f3a094d1db142b7682d6
Evolusen <31206344+Evolusen@users.noreply.github.com>
2017-09-07 22:53:03 +0200

    Add files via upload

---
+++ src/CMakeFiles/CryptoNoteCore.dir/DependInfo.cmake
@@ -0,0 +1,55 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/evolusen/una/src/CryptoNoteCore/Account.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Account.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/BlockIndex.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/BlockIndex.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/Blockchain.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Blockchain.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/BlockchainIndices.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/BlockchainIndices.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/BlockchainMessages.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/BlockchainMessages.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/Checkpoints.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Checkpoints.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/Core.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Core.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/CoreConfig.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/CoreConfig.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/CryptoNoteBasic.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/CryptoNoteBasic.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/CryptoNoteBasicImpl.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/CryptoNoteBasicImpl.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/CryptoNoteFormatUtils.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/CryptoNoteFormatUtils.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/CryptoNoteSerialization.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/CryptoNoteSerialization.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/CryptoNoteTools.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/CryptoNoteTools.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/Currency.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Currency.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/Difficulty.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Difficulty.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/IBlock.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/IBlock.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/ITimeProvider.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/ITimeProvider.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/Miner.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Miner.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/MinerConfig.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/MinerConfig.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/SwappedMap.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/SwappedMap.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/SwappedVector.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/SwappedVector.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/Transaction.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/Transaction.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/TransactionExtra.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/TransactionExtra.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/TransactionPool.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/TransactionPool.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/TransactionPrefixImpl.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/TransactionPrefixImpl.cpp.o"
+  "/home/evolusen/una/src/CryptoNoteCore/TransactionUtils.cpp" "/home/evolusen/una/build/release/src/CMakeFiles/CryptoNoteCore.dir/CryptoNoteCore/TransactionUtils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "STATICLIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../external"
+  "version"
+  "../../src/Platform/Linux"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
