6e02488d654f89568d62839c85b7c16d3a6c9b6a
UnA <31206344+Evolusen@users.noreply.github.com>
2017-11-05 21:02:50 +0200

    Add files via upload

--- src/CTestTestfile.cmake
+++ src/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: /home/evolusen/una/src
-# Build directory: /home/evolusen/una/build/release/src
+# Source directory: /home/evolusen/cryptonote/src
+# Build directory: /home/evolusen/cryptonote/build/release/src
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
